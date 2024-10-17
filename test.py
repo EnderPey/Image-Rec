ae926f5cc5ae7553a03b2a200864e58662267671
Ender <ender@peyzner.com>
2024-10-16 20:24:41 -0600

    Got everything to work

--- test.py
+++ test.py
@@ -16,36 +16,36 @@ import os
 
 
 
-# class TestModels:
-#     def __init__(self, h5_address:str, GPU=True):
-#         self.exps = ['Neutral', 'Happy', 'Sad', 'Surprise', 'Fear', 'Disgust', 'Anger']
+class TestModels:
+    def __init__(self, h5_address:str, GPU=True):
+        self.exps = ['Neutral', 'Happy', 'Sad', 'Surprise', 'Fear', 'Disgust', 'Anger']
 
-#         if GPU:
-#             physical_devices = tf.config.list_physical_devices('GPU')
-#             tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        if GPU:
+            physical_devices = tf.config.list_physical_devices('GPU')
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-#         self.model = self.load_model(h5_address=h5_address)
+        self.model = self.load_model(h5_address=h5_address)
 
-#     def load_model(self, h5_address: str):
-#         "load weight file and create the model once"
-#         model = tf.keras.models.load_model(h5_address, custom_objects={'tf': tf})
-#         return model
+    def load_model(self, h5_address: str):
+        "load weight file and create the model once"
+        model = tf.keras.models.load_model(h5_address, custom_objects={'tf': tf})
+        return model
 
-#     def recognize_fer(self, img_path:str):
-#         "create and image from the path and recognize expression"
-#         img = imread(img_path)
-#         # resize img to 1*224*224*3
-#         img = resize(img, (224, 224,3))
-#         img = np.expand_dims(img, axis=0)
-#         #
-#         prediction = self.model.predict_on_batch([img])
-#         exp = np.array(prediction[0])
-#         '''in case you need the embeddings'''
-#         # embeddings = prediction[1:]
-#         print(self.exps[np.argmax(exp)])
+    def recognize_fer(self, img_path:str):
+        "create and image from the path and recognize expression"
+        img = imread(img_path)
+        # resize img to 1*224*224*3
+        img = resize(img, (224, 224,3))
+        img = np.expand_dims(img, axis=0)
+        #
+        prediction = self.model.predict_on_batch([img])
+        exp = np.array(prediction[0])
+        '''in case you need the embeddings'''
+        # embeddings = prediction[1:]
+        print(self.exps[np.argmax(exp)])
 
 
-# if __name__ == '__main__':
-#     print("Test")
-#     test = TestModels(h5_address='AffectNet_6336.h5', GPU=False)
-#     test.recognize_fer('img.png')
+if __name__ == '__main__':
+    print("Test")
+    test = TestModels(h5_address='AffectNet_6336.h5', GPU=False)
+    test.recognize_fer('img.jpg')
